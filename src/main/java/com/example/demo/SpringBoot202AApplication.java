e4a1597b904e9a1f73276277209915528ef27975
rcorcios023 <56006220+rcorcios023@users.noreply.github.com>
2019-10-28 19:07:22 -0400

    lesson_202A

---
+++ src/main/java/com/example/demo/SpringBoot202AApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot202AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot202AApplication.class, args);
+    }
+
+}
